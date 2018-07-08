710826d546ca4ad883900d105ac73a5126729809
agung <M Agung Sutrisno>
2018-07-08 11:13:13 +0700

    Sample Component with function

--- App.js
+++ App.js
@@ -1,13 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
 
-export default class App extends Component {
-
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Hello From React</Text>
-      </View>
-    )
-  }
-}
+export default App = () => (
+  <View style={{ flex:1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Hello KSL Pamekasan</Text>
+  </View>
+)
