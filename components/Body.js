6b6b745b3bd348f59ec25b481e80c7f8043bba70
agung <M Agung Sutrisno>
2018-07-11 00:08:13 +0700

    Checkbox with event handling

--- components/Body.js
+++ components/Body.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, CheckBox } from 'react-native';
+import { View, Text, StyleSheet, CheckBox, Alert } from 'react-native';
 
 import { Content1, Content2 } from "./Content";
 
 export default class Body extends Component {
+
+  handleCheckbox(val) {
+    alert("Tested checked "+val);
+  }
 
   render() {
     return (
       <View style={ styles.container }>
         <View style={ styles.listItem }>
           <View style={ styles.listItemLeft }>
-            <CheckBox />
-            <CheckBox />
+            <CheckBox onChange={() => this.handleCheckbox(1)} />
+            <CheckBox onChange={ () => this.handleCheckbox(2) } />
           </View>
           <View style={ styles.listItemRight }>
             <Text style={{ padding:7 }}>List item one</Text>
