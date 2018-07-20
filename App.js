e37ac3d1699f11e46d966fda251a792c6871442c
agung <M Agung Sutrisno>
2018-07-20 21:49:02 +0700

    Sample state with Flatlist

--- App.js
+++ App.js
@@ -9,22 +9,29 @@ import TodoItem from "./components/TodoItem";
 
 export default class App extends Component {
 
-  todos = [
-    {
-      id: 1,
-      note: 'First Item'
-    },
-    {
-      id: 2,
-      note: 'Second Item '
-    },
-    {
-      id: 3,
-      note: 'Third Item'
-    }
-  ];
+  state = {
+    count: 0,
+    todos: []
+  }
 
   _keyExtractor = (item, index) => item.id.toString();
+
+  handleIncTodo() {
+
+    count = this.state.count+1;
+    todos = this.state.todos;
+    todos.push({
+        id: count,
+        note: 'Todo Item #'+count
+    })
+
+    this.setState({
+      todos: todos,
+      count: count
+    })
+
+    console.log(todos)
+  }
 
   render() {
     return(
@@ -37,7 +44,7 @@ export default class App extends Component {
             </Button>
           </Left>
           <Body>
-            <Title>Not To Do Lists</Title>
+            <Title>Not To Do Lists ({this.state.count})</Title>
           </Body>
           <Right/>
         </Header>
@@ -45,13 +52,22 @@ export default class App extends Component {
         <Content>
           <List>
             <FlatList
-              data={this.todos}
+              data={this.state.todos}
+              extraData={this.state}
               keyExtractor={this._keyExtractor}
               renderItem={({item}) => <TodoItem todo={item} />}
             />
-            {/* {this.todos.map((todo) => <TodoItem todo={todo} key={todo.id} />)} */}
+            {/* {this.state.todos.map((todo) => <TodoItem todo={todo} key={todo.id} />)} */}
           </List>
         </Content>
+
+        <Fab
+          style={{ backgroundColor: '#5067FF' }}
+          position="bottomRight"
+          onPress={() => this.handleIncTodo() }>
+          <Icon name="add" />
+        </Fab>
+
       </Container>
     )
   }
